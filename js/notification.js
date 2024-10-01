244f63e131fa915e7ec05f59ac7df939674107b0
DiegoHidalgo02 <hidalgo.moreno.diego@gmail.com>
2024-10-01 15:32:52 +0200

    Refactor dropdown menu width, add close button to notification header, adjust notification list markup, and improve notification generation logic

--- js/notification.js
+++ js/notification.js
@@ -2,7 +2,13 @@ const alertSection = document.querySelector("#alert");
 const bell = document.querySelector(".bell > svg");
 const notificationPoint = document.querySelector("#notification");
 const dropMenu = document.querySelector("#ndropDownMenu");
+const dropMenuList = document.querySelector("#dropMenuList")
+const markAllRead = document.querySelector("#markAllRead")
 const closeNotification = document.querySelector("#closeNotification");
+let notificationTextGenerated = [];
+let notificationUnreaded = [];
+let notificationReaded = [];
+
 
 const notification = [
   "New activity recorded",
@@ -15,12 +21,52 @@ const notification = [
   "New insights generated.",
 ];
 
-function randomNotification() {
-  const noticationText =
-    notification[Math.floor(Math.random() * notification.length)];
 
-  return noticationText;
+
+function randomNotification() {
+
+  let notificationText = "";
+  do{
+    notificationText = notification[Math.floor(Math.random() * notification.length)];
+  }while(notificationTextGenerated.includes(notificationText))
+  
+  notificationTextGenerated.push(notificationText);
+  return notificationText;
+
 }
+
+function generateNotification(){
+
+  const notification = document.createElement("li")
+  const checkNotification = document.createElement("span");
+
+  checkNotification.classList.add("material-symbols-rounded");
+  checkNotification.textContent = "check_small";
+  checkNotification.id = "checkNotification"
+
+  notification.textContent = randomNotification();
+  notification.appendChild(checkNotification);
+  notification.id = "notify"
+
+  notificationUnreaded.push(notification)
+  dropMenuList.appendChild(notification);
+  notificationPoint.style.visibility = "visible";
+}
+
+setTimeout(() => {
+  for (let i = 0; i < 2; i++) {
+    generateNotification();
+  }
+  const checkNotification = document.querySelector("#checkNotification")
+
+}, 10000);
+
+setInterval(() => {
+  if(notification.length !== notificationTextGenerated.length){
+    generateNotification();
+  }
+
+}, 180000);
 
 const banner = document.createElement("div");
 const paragraph = document.createElement("p");
@@ -45,15 +91,45 @@ spanClose.addEventListener("click", ()=>{
     banner.style.display = "none";
 })
 
+bell.addEventListener("click", () => {
+    dropMenu.style.display = "block";
+});
+
 closeNotification.addEventListener("click", () => {
   if(dropMenu.style.display === "block"){
     dropMenu.style.display = "none";
   }
 })
 
-bell.addEventListener("click", () => {
+markAllRead.addEventListener("click", ()=>{
+  dropMenuList.innerHTML = '';
+
+  notificationUnreaded.forEach(e =>{
+    notificationReaded.push(e);
+  });
+
+  for(let i = 0; i < notificationReaded.length; i++){
+    notificationUnreaded.pop();
+  }
+  
+  if(notificationUnreaded.length === 0){
     notificationPoint.style.visibility = "hidden";
-    dropMenu.style.display = "block";
-});
+  }
+
+})
+
+
+dropMenuList.addEventListener("click", e =>{
+
+  if(e.target.tagName === "LI"){
+    e.target.remove();
+    notificationReaded.push(e);
+    notificationUnreaded.pop();
+    if(notificationUnreaded.length === 0){
+      notificationPoint.style.visibility = "hidden";
+    }
+  }
+
+})
 
 
