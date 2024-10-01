04b8e0683213ae80dc20c03d1fd5187d19b65ad5
Diego Hidalgo <Hidalgo.moreno.diego@gmail.com>
2024-10-01 22:02:42 +0200

    Add userMessage.js script to handle sending messages

---
+++ js/userMessage.js
@@ -0,0 +1,7 @@
+const SendButton = document.querySelector("#send");
+const search = document.querySelector("#search");
+const textArea = document.querySelector("#message");
+
+SendButton.addEventListener("click", ()=>{
+
+})
