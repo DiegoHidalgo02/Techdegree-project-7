4b2cd70094fd0d0959e735efa4d04ed1f53f867f
DiegoHidalgo02 <hidalgo.moreno.diego@gmail.com>
2024-10-02 09:36:30 +0200

    Refactor userMessage.js to handle empty search and text area inputs

--- js/userMessage.js
+++ js/userMessage.js
@@ -4,4 +4,22 @@ const textArea = document.querySelector("#message");
 
 SendButton.addEventListener("click", ()=>{
 
+    if(search.value === "" || textArea.value === ""){
+
+        const paragraph = document.createElement("p");
+        const icon = document.createElement("span");
+
+        if(search.value === ""){
+            paragraph.textContent = "Error: search field empty";
+        }else if(textArea.value === ""){
+            paragraph.textContent = "Error: text area empty"
+        }else{
+            paragraph.textContent= "Error: search field and text area are empty"
+        }
+        
+        icon.classList.add("");
+        icon.textContent = "";
+        paragraph.prepend(icon);
+    }
+
 })
