20f81fe6b6befb448ec4d9c0c7dfab6b10a25655
Diego Hidalgo <Hidalgo.moreno.diego@gmail.com>
2024-10-02 10:53:29 +0200

    Refactor userMessage.js to handle empty search and text area inputs

--- js/userMessage.js
+++ js/userMessage.js
@@ -1,25 +1,58 @@
 const SendButton = document.querySelector("#send");
 const search = document.querySelector("#search");
 const textArea = document.querySelector("#message");
+const messageHeader = document.querySelector(".headerSectionMessage");
+let ErrorOnDisplay = false;
+let succesOnDisplay = false;
+const paragraph = document.createElement("p");
+const icon = document.createElement("span");
 
 SendButton.addEventListener("click", ()=>{
 
-    if(search.value === "" || textArea.value === ""){
-
-        const paragraph = document.createElement("p");
-        const icon = document.createElement("span");
-
-        if(search.value === ""){
+    if (search.value === "" || textArea.value === "") {
+        if (search.value === "" && textArea.value !== "") {
             paragraph.textContent = "Error: search field empty";
-        }else if(textArea.value === ""){
-            paragraph.textContent = "Error: text area empty"
-        }else{
-            paragraph.textContent= "Error: search field and text area are empty"
+        } else if (textArea.value === "" && search.value !== "") {
+            paragraph.textContent = "Error: text area empty";
+        } else {
+            paragraph.textContent = "Error: search field and text area are empty";
         }
-        
-        icon.classList.add("");
-        icon.textContent = "";
-        paragraph.prepend(icon);
-    }
 
+        icon.classList.add("material-symbols-outlined");
+        icon.textContent = "warning";
+        paragraph.prepend(icon);
+
+        if(paragraph.classList.contains("succes")){
+            paragraph.classList.remove("succes")
+        }
+
+        paragraph.classList.add("error");
+        messageHeader.appendChild(paragraph);
+        ErrorOnDisplay = true;
+        succesOnDisplay = false;
+
+     } else if(succesOnDisplay === false) {
+
+        paragraph.textContent = "Message sent";
+        icon.classList.add("material-symbols-outlined");
+        icon.textContent = "check";
+        paragraph.prepend(icon);
+
+        if(paragraph.classList.contains("error")){
+            paragraph.classList.remove("error")
+        }
+
+        paragraph.classList.add("succes")
+        messageHeader.appendChild(paragraph);
+        ErrorOnDisplay = false
+        succesOnDisplay = true;
+    }
 })
+
+setInterval(() => {
+    if(ErrorOnDisplay === true || succesOnDisplay === true){
+        messageHeader.querySelector("p").remove();
+        ErrorOnDisplay = false;
+        succesOnDisplay = false;
+    }
+}, 90000);
