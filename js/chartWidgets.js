d8e4e1f56bbe8fb07916401f4c9cc0c99208c04d
Diego Hidalgo <Hidalgo.moreno.diego@gmail.com>
2024-10-10 19:38:09 +0200

    Created mobileChart and uploaded

--- js/chartWidgets.js
+++ js/chartWidgets.js
@@ -100,3 +100,37 @@ let dailyChart = new Chart(dailyCanvas, {
 
 });
 
+let mobileChart = new Chart(mobileCanvas,{
+
+    type: 'doughnut',
+
+    data:{
+
+        labels: ["Desktop", "Tablet", "Phones"],
+        datasets: [{
+
+            label: '# of Users',
+            data: [2000, 550, 500],
+            borderWidth: 0,
+            backgroundColor: ['#7477BF',
+                              '#78CF82',
+                              '#51B6C8']
+        }]
+
+    },
+
+    options:{
+        aspectRatio: 1.9,
+        plugins: {
+            legend: {
+                position: 'right',
+                labels: {
+                    boxWidth: 20,
+                    fontStyle: 'bold'
+                }
+            }
+        }
+    }
+
+})
+
