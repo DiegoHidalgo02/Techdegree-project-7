18d0df58f8f9331f08d2e30122a3369aeee8265c
Diego Hidalgo <Hidalgo.moreno.diego@gmail.com>
2024-10-10 20:17:07 +0200

    Get chart widgets for traffic, daily, and mobile users

---
+++ js/chartWidgets.js
@@ -0,0 +1,3 @@
+const trafficCanvas = document.querySelector("#traffic-chart").getContext('2d');
+const dailyCanvas = document.querySelector("#daily-chart").getContext('2d');
+const mobileCanvas = document.querySelector("#mobile-user").getContext('2d');
